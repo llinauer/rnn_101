bf174fbeb8fda2cdf94ee5f9fd0a491693ba496f
llinauer <l.linauer@gmx.at>
2024-12-18 17:44:28 +0100

    Add train.py

---
+++ rnn/train.py
@@ -0,0 +1,256 @@
+"""
+train.py
+
+Train an RNN to predict the digit sum of a sequence
+"""
+
+import random
+from pathlib import Path
+import einops
+import torch
+from torch import nn
+import torch.nn.functional as F
+from data import VOCAB_SIZE, DigitSequenceDataset, EOS_IDX, EOA_IDX
+from torch.utils.tensorboard import SummaryWriter
+import hydra
+from omegaconf import DictConfig
+from torch.utils.data import DataLoader, random_split
+from tqdm import tqdm
+
+
+class OneHotCrossEntropyLoss(nn.Module):
+    """ Custom class for calculating Cross entropy with one-hot encoded targets """
+    def forward(self, logits, one_hot_targets):
+        log_probs = F.log_softmax(logits, dim=-1)
+        cross_entropy = -einops.einsum(log_probs, one_hot_targets,
+                                       "batch sequence vocab, batch sequence vocab -> batch sequence")
+        loss = cross_entropy.mean()
+        return loss
+
+
+class DigitSumModel(nn.Module):
+    """ RNN that takes a sequence of digits as an input and predicts their sum """
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+
+        # define layers
+        self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+        self.h2o = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, h_0=None):
+        hidden, _ = self.rnn(x, h_0)
+        logits = self.h2o(hidden)
+
+        return logits, hidden
+
+
+def sample_from_rnn(model, input_sequence, max_seq_len=5):
+    """ Sample from an RNN, by giving it an input_sequence and iteratively generate new tokens
+        input_sequence is of shape (n_sequence, d_vocab) """
+
+    # pass input_sequence through model
+    with torch.no_grad():
+        next_logits, hidden_states = model(input_sequence)
+    # process the logits of the last input, to get the next input token
+    next_token = F.one_hot(next_logits[-1].argmax(), num_classes=VOCAB_SIZE).float()
+    generated_tokens = [next_token]
+    hidden_states = hidden_states[-1:]
+
+    if torch.equal(next_token, F.one_hot(torch.tensor(EOA_IDX)).float()):
+        return torch.vstack(generated_tokens)
+
+    # loop until the max number of generated tokens is reached or we encounter an EOA token
+    for _ in range(max_seq_len - 1):
+
+        # put tokens in rnn
+        next_logits, hidden_states = model(next_token.unsqueeze(0), hidden_states)
+        next_token = F.one_hot(next_logits[-1].argmax(), num_classes=VOCAB_SIZE).float()
+
+        generated_tokens.append(next_token)
+
+        # check if EOA token was generated
+        if torch.equal(next_token, F.one_hot(torch.tensor(EOA_IDX)).float()):
+            break
+
+    return torch.vstack(generated_tokens)
+
+
+def check_sequence_correctness(input_sequence: torch.Tensor, answer: str) -> bool:
+    """ Check if the generated answer for the input_sequence is correct
+        The shape of input_sequence is (n_sequence, d_vocab) """
+    
+    # calc sum of input_sequence tokens, ignore the EOS token
+    digit_sum = input_sequence.argmax(dim=1)[:-1].sum().item()
+    answer = answer.replace(" ", "").replace("EOA", "")
+    # if the answer is just EOA, set to 0
+    if not answer:
+        answer = 0
+    else:
+        answer = int(answer)
+    return digit_sum == answer
+
+
+
+def translate_tokens(tokens: torch.Tensor) -> str:
+    """ Translate the tokens back to the vocab and print
+        tokens is of shape (n_tokens, d_vocab) """
+
+    # convert sequence of tensor to list of integers
+    digit_list = tokens.argmax(dim=1).tolist()
+    
+    # join the list to a string
+    digit_string = " ".join(map(str, digit_list))
+
+    # replace 10 with EOA and 11 with EOS
+    digit_string = digit_string.replace(str(EOA_IDX), "EOA")
+    digit_string = digit_string.replace(str(EOS_IDX), "EOS")
+    return digit_string
+
+
+def calc_accuracy(generated_sequence, target_sequence):
+    """ Compare how good a generated sequence fits a target sequence"""
+    pass
+    
+
+
+def train(model, train_loader, val_loader, loss_func, optimizer, n_epochs, tb_logger):
+    """ Train function. Iterate over the batch_loader epochs times and train the model.
+        Log metrics with tensorboard logger """
+
+
+    for epoch in range(n_epochs):
+
+        train_loss = 0.0
+        val_loss = 0.0
+        best_model_performance = -torch.inf
+
+        # train loop
+        # move model to appropriate device and set to train mode
+        model.train()
+
+        # create progress bar out of train_loader
+        train_progress_bar = tqdm(enumerate(train_loader), total=len(train_loader),
+                                  desc=f"Epoch {epoch+1}/{n_epochs}")
+        
+        for _, (input_sequences, target_sequences) in train_progress_bar:
+
+            # reset gradients
+            optimizer.zero_grad()
+
+            # pass input_sequences through model to get the first prediction and the hidden state
+            input_logits, input_hidden_states = model(input_sequences)
+
+            # pass the target sequences through the model to get further predictions
+            target_logits, _ = model(
+                target_sequences, h_0=input_hidden_states[:, -1, :].unsqueeze(0)
+                )
+
+            # calculate the loss
+            # first, take the prediction of the RNN at the end of the input_sequence 
+            # -> should be equal to the first element of the target_sequence
+            loss = loss_func(input_logits[:, -2:-1, :], target_sequences[:, 0:1, :])
+
+            # for each prediction in the target sequence, compare with the following elements
+            # of the target sequence
+            loss += loss_func(target_logits[:, :-1, :], target_sequences[:, 1:, :])
+
+            # backward pass and optimizer step
+            loss.backward()
+            optimizer.step()
+
+            # accumulate training loss
+            train_loss += loss.item()
+
+            # print running training loss
+            train_progress_bar.set_postfix({"Loss": f"{loss.item():.4f}"})
+
+        if epoch % 10 == 0:
+            print(f"Epoch [{epoch+1}/{n_epochs}], Average Loss: {train_loss/len(train_loader):.4f}")
+
+        # validation loop, set model to eval mode
+        model.eval()
+
+        # create progress bar out of val_loader
+        val_progress_bar = tqdm(enumerate(val_loader), total=len(val_loader))
+        for _, (input_sequences, target_sequences) in val_progress_bar:
+
+            # pass input_sequences and target_sequences through the model
+            input_logits, input_hidden_states = model(input_sequences)
+            # pass the target sequences through the model to get further predictions
+            target_logits, _ = model(
+                target_sequences, h_0=input_hidden_states[:, -1, :].unsqueeze(0)
+                )
+            
+            # calculate loss like in training loop
+            loss = loss_func(input_logits[:, -2:-1, :], target_sequences[:, 0:1, :])
+            loss += loss_func(target_logits[:, :-1, :], target_sequences[:, 1:, :])
+            val_loss += loss.item()
+
+        model_performance = -val_loss / len(val_loader)
+        #if mean_val_loss > best_model_performance:
+            #print(f"New best model performance. Saving model to {log_path/'best_model.pth'}")
+            #torch.save(model.cpu().state_dict(), log_path / "best_model.pth")
+
+        if epoch % 1 == 0:
+            print(f"Validation: Average Loss: {val_loss/len(val_loader):.4f}")
+            # check current model output on a randomly sampled sequence from the validation set
+            random_batch_idx = random.randint(0, len(val_loader)-1)
+            random_batch = list(val_loader)[random_batch_idx][0]
+            random_sample_idx = random.randint(0, len(random_batch)-1)
+            random_sample = random_batch[random_sample_idx, :, :]
+            generated_tokens = sample_from_rnn(model, random_sample)
+            
+            # print the sequence and the generated tokens
+            input_seq_str = translate_tokens(random_sample)
+            print("Input sequence: ", input_seq_str)
+            answer_str = translate_tokens(generated_tokens)
+            print("Answer: ", answer_str)
+            print(check_sequence_correctness(random_sample, answer_str))
+
+
+
+
+@hydra.main(version_base=None, config_name="config", config_path=".")
+def main(cfg: DictConfig) -> None:
+    """ Main function """
+
+    logger = SummaryWriter()
+
+    # check if dataset path exists and load data if it does
+    if not cfg.dataset_path:
+        print("Please provide path to dataset with the 'dataset_path' argument")
+        return
+
+    if not Path(cfg.dataset_path).exists():
+        print("Dataset at {ds_path} does not exist")
+        return
+    
+    ds = DigitSequenceDataset(cfg.dataset_path)
+
+    # split dataset into training and validation
+    train_set_len = int(len(ds) * cfg.train_split_fraction)
+    train_ds, val_ds = random_split(ds, [train_set_len, len(ds) - train_set_len])
+
+    # create dataloaders
+    train_loader = DataLoader(train_ds, batch_size=cfg.batch_size, num_workers=8,
+                              shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=cfg.batch_size, num_workers=8,
+                            shuffle=False)
+    
+    # create rnn model
+    rnn = DigitSumModel(VOCAB_SIZE, 128, VOCAB_SIZE)
+    
+    # define optimizer 
+    optim = torch.optim.Adam(rnn.parameters(), lr=cfg.learning_rate)
+    
+    # use custom loss function
+    loss_func = OneHotCrossEntropyLoss()
+
+    train(rnn, train_loader, val_loader, loss_func, optim, cfg.n_epochs, logger)
+    
+
+
+
+if __name__ == "__main__":
+    main()
