964dfa16bc10780b1da5553c6985aa90f489535c
llinauer <l.linauer@gmx.at>
2024-12-29 23:32:24 +0100

    Change return values of DigitSumModel forward method, to logits and last_hidden state; allows using RNN and LSTM interchangeably

--- rnn/model.py
+++ rnn/model.py
@@ -8,22 +8,21 @@ from torch import nn
 
 
 class DigitSumModel(nn.Module):
-    """ RNN that takes a sequence of digits as an input and predicts their sum """
+    """ RNN that takes a sequence of digits as an input and predicts their sum.
+        Uses either fully-connected RNN or LSTM"""
 
-    def __init__(self, input_size, hidden_size, output_size, rnn_type="rnn"):
+    def __init__(self, input_size, hidden_size, output_size, model_type="rnn"):
         super().__init__()
 
-        if rnn_type == "lstm":
-            rnn_layer = nn.LSTM
-        else:
-            rnn_layer = nn.RNN
-
         # define layers
-        self.rnn = rnn_layer(input_size, hidden_size, batch_first=True)
+        if model_type == "lstm":
+            self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
+        else:
+            self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
         self.h2o = nn.Linear(hidden_size, output_size)
 
     def forward(self, x, h_0=None):
-        hidden, _ = self.rnn(x, h_0)
-        logits = self.h2o(hidden)
+        output, last_hidden = self.rnn(x, h_0)
+        logits = self.h2o(output)
 
-        return logits, hidden
+        return logits, last_hidden
